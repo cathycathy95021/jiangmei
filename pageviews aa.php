4a0aa0f21496745db45f8287607f127ced285483
cathycathy95021 <73523807+cathycathy95021@users.noreply.github.com>
2023-01-14 20:33:22 +0800

    Delete pageviews aa.php

--- pageviews aa.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$fp = fopen ( '../pageviews.ogg' , 'r' );//原先數字1634
-   $num = fgets ( $fp );
-   fclose( $fp );
-   $fpo = fopen ( '../pageviews.ogg' , 'w' );
-   fwrite( $fpo , ++$num );
-   fclose( $fpo );
-?>
